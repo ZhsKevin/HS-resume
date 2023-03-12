4fc6683eb5ff9a1b67da55026a7bb2af9cf9d1cb
zhhsh2021 <zhhsh2021@123.com>
2023-03-13 00:40:21 +0800

    2023/03/13 0:40:18

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   output: {
-    publicPath: '',
+    publicPath: '/',
     filename: '[name].js',
   },
   resolve: {
